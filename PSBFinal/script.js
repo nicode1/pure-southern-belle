bb9a8f0a50b33605be629dac0218131c8d5789e0
nicode1 <60286543+nicode1@users.noreply.github.com>
2020-01-28 15:07:04 +0400

    Add files via upload

---
+++ PSBFinal/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".typed", {
+        strings: ["Buy your Valentine's Day gifts!"],
+        typeSpeed: 125,
+        loop: true,
+        startDelay: 950,
+        showCursor: false
+    });
